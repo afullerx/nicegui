a0fdb67794f35943823496a8777f16aec2a96f51
aafuller <aafuller7@outlook.com>
2024-07-01 16:30:17 -0400

    refactor

--- nicegui/static/nicegui.js
+++ nicegui/static/nicegui.js
@@ -315,7 +315,7 @@ function createApp(elements, options) {
               console.log("reloading because handshake failed for clientId " + window.clientId);
               window.location.reload();
             } else if (!res.success && res.reason === "sync_failure") {
-              console.log("reloading because sync failed (see message_history_max)");
+              console.log("reloading because client could not be synchronized with the server");
               window.location.reload();
             }
             document.getElementById("popup").ariaHidden = true;
@@ -361,14 +361,12 @@ function createApp(elements, options) {
         sync: (msg) => {
           if (msg.sync_id == window.socket.id) {
             window.syncing = false;
-            const len = msg.history.length;
-            const history = msg.history;
-            for (let i = 0; i < len; i++) {
-              let message = history[i][1];
+            for (let i = 0; i < msg.history.length; i++) {
+              let message = msg.history[i][1];
               if (message.message_id > window.lastMessageId) {
                 window.lastMessageId = message.message_id;
                 delete message.message_id;
-                messageHandlers[history[i][0]](message);
+                messageHandlers[msg.history[i][0]](message);
               }
             }
           }
